f9bf83e81334bcb046ee51947ea5ce5b868be88f
Duong Tien <4401104193@student.hcmue.edu.vn>
2020-06-19 00:54:44 +0700

    Cleanning up

--- Sudoku/index.js
+++ Sudoku/index.js
@@ -7,10 +7,5 @@ import App from './App';
 import {name as appName} from './app.json';
 
 import Main from './Main';
-//import Cell from './component/Cell';
-//import CellOld from './CellOld';
-import Grid from './component/Grid';
-//import StackCell from './component/StackCell';
-import Stack from './component/Stack';
 
 AppRegistry.registerComponent(appName, () => Main);
